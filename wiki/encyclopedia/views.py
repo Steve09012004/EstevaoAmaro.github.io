2027c5b0b4692763d85928c3b6d43a107fdb34c3
Steve09012004 <stvviking@gmail.com>
2025-06-02 21:11:42 -0300

    Adding the funcional search system

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,9 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.http import HttpResponseRedirect
 
 from . import util
 
 
 def index(request):
+    if request.method == 'POST':
+        searchValue = request.POST['q'].strip()
+        for iten in util.list_entries():
+            if searchValue.lower() == iten.lower():
+                return HttpResponseRedirect(reverse('encyclopedia:view', args = [iten]))
+            
+        entries =[]
+        for iten in util.list_entries():
+            if searchValue.lower() in iten.lower():
+                entries += [iten]
+
+        return render(request, "encyclopedia/results.html",{
+            'entries':entries,
+            'searchValue': searchValue
+        })
+
+        
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
